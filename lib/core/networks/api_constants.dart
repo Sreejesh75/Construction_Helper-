f55cbf31186717a3e2f054b04394b28f4f4143db
EQsoftsreejesh <eqsoftsreejesh@gmail.com>
2026-02-02 13:51:59 +0530

    changed localhost and used hostapi using render

--- lib/core/networks/api_constants.dart
+++ lib/core/networks/api_constants.dart
@@ -1,5 +1,9 @@
 class ApiConstants {
-  static const String baseUrl = "http://10.0.2.2:3000";
+  static const String baseUrl = 
+  
+  "https://api-construction-ehe5.onrender.com";
+  
+  // "http://10.0.2.2:3000";
 
   // User APIs
   static const String createUser = "/api/create-user";
@@ -7,21 +11,21 @@ class ApiConstants {
 
   // Project APIs
   static const String createProject = "/api/create-project";
-  static const String getProjects = "/api/projects"; // /:userId
-  static const String updateProject = "/api/update-project"; // /:projectId
-  static const String deleteProject = "/api/delete-project"; // /:projectId
+  static const String getProjects = "/api/projects"; 
+  static const String updateProject = "/api/update-project"; 
+  static const String deleteProject = "/api/delete-project"; 
 
   //  Material APIs
   static const String addMaterial = "/api/add-material";
-  static const String getMaterials = "/api/materials"; // /:projectId
-  static const String updateMaterial = "/api/update-material"; // /:materialId
-  static const String deleteMaterial = "/api/delete-material"; // /:materialId
+  static const String getMaterials = "/api/materials"; 
+  static const String updateMaterial = "/api/update-material"; 
+  static const String deleteMaterial = "/api/delete-material"; 
 
   // Summary API
-  static const String projectSummary = "/api/project-summary"; // /:projectId
+  static const String projectSummary = "/api/project-summary";
   
   // Document APIs
   static const String uploadDocument = "/api/upload-document";
-  static const String getDocuments = "/api/documents"; // /:projectId
-  static const String deleteDocument = "/api/delete-document"; // /:documentId
+  static const String getDocuments = "/api/documents"; 
+  static const String deleteDocument = "/api/delete-document"; 
 }
